641dd82be40e06d1ca84d5158e760770725c309a
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-11-20 12:43:18 -0500

    edit account page working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :location, :f_name, :l_name, :description)
   end
 end
